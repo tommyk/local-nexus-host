aa8003a413a071a546f15ff72487e1e06437f138
Tommy Kelly <tommy.kelly@wendys.com>
2016-10-05 16:35:43 -0400

    crucial piece was commented out :\

--- build_nexus_host.sh
+++ build_nexus_host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-#docker-machine create -d virtualbox nexus-host
+docker-machine create -d virtualbox nexus-host
 docker-machine ssh nexus-host "sudo mkdir /nexus-data && sudo chown -R 200 /nexus-data"
 
 #setup hosts file 
