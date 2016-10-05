32e21c5c1bd764d2a3d8a2639d09f4e2c35c49b2
Tommy Kelly <tommy.kelly@wendys.com>
2016-10-04 20:41:03 -0400

    added code to setup /etc/hosts

--- build_nexus_host.sh
+++ build_nexus_host.sh
@@ -3,6 +3,11 @@ set -e
 
 #docker-machine create -d virtualbox nexus-host
 docker-machine ssh nexus-host "sudo mkdir /nexus-data && sudo chown -R 200 /nexus-data"
+
+#setup hosts file 
+#TODO: let user know what is happening before putting in password
+sudo bash -c "echo \"$(docker-machine ip nexus-host)  nexushost\" >> /etc/hosts"
+
 docker-machine env nexus-host
 eval $(docker-machine env nexus-host)
 docker-compose build
