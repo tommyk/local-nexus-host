b9470f2433e56c1e3169655efe5bbc19b0d00572
Tommy Kelly <tommy.kelly@wendys.com>
2016-10-06 01:05:28 -0400

    two commands seem to be working better, not sure why

--- build_nexus_host.sh
+++ build_nexus_host.sh
@@ -3,6 +3,7 @@ set -e
 
 docker-machine create -d virtualbox nexus-host
 docker-machine ssh nexus-host "sudo mkdir /nexus-data && sudo chown -R 200 /nexus-data"
+docker-machine ssh nexus-host "sudo chown -R 200 /nexus-data"
 
 #setup hosts file 
 #TODO: let user know what is happening before putting in password
