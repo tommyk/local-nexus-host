78d812ce4e8874a15886eec5bb344afef36b780d
Tommy Kelly <tommy.kelly@wendys.com>
2016-10-08 14:12:18 -0400

    moved to using a docker volume and not using hosts folder

--- build_nexus_host.sh
+++ build_nexus_host.sh
@@ -2,11 +2,10 @@
 set -e
 
 docker-machine create -d virtualbox nexus-host
-docker-machine ssh nexus-host "sudo mkdir /nexus-data && sudo chown -R 200 /nexus-data"
-docker-machine ssh nexus-host "sudo chown -R 200 /nexus-data"
 
 #setup hosts file 
 #TODO: let user know what is happening before putting in password
+#TODO: get a fixed IP address...
 sudo bash -c "echo \"$(docker-machine ip nexus-host)  nexushost\" >> /etc/hosts"
 
 docker-machine env nexus-host
